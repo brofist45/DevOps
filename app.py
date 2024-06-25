028880b3655b2a6f2699cd106fbdd9be3f344604
brofist45 <163131350+brofist45@users.noreply.github.com>
2024-06-25 22:56:09 +0530

    New Files

--- app.py
+++ app.py
@@ -2,24 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     print("This is the home page.")
     return "Welcome to the home page!"
-
 
 @app.route('/login')
 def login():
     print("This is the login page.")
     return "ffefewf."
 
-
 @app.route('/details')
 def details():
     print("This is the details page.")
     return "This is the details page."
 
+@app.route('/new-feature')
+def details():
+    print("This is the new feature page.")
+    return "This is the new feature page."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5007)
+    app.run(host='0.0.0.0',port=5007)
+
+
